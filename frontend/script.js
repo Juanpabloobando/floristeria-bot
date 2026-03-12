b337d21e0411955baa719f57d46a99ca5d9bb90c
Juan Pablo Obando Alvarez <jp.obandoa1@uniandes.edu.com>
2026-03-12 12:02:28 -0500

    conectar frontend con backend publico

--- frontend/script.js
+++ frontend/script.js
@@ -24,7 +24,7 @@ async function sendMessage(customText = null) {
   input.value = "";
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/chat", {
+    const response = await fetch("https://floristeria-bot.onrender.com/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
